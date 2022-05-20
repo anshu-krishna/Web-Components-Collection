ec6ed275f7114f2198241ea3ca18054842dc5342
Anshu Krishna <anshu.krishna5@gmail.com>
2022-05-20 12:44:08 +0530

    component updated to new design

--- tabs/src/tab-container.js
+++ tabs/src/tab-container.js
@@ -1,44 +1,47 @@
-class TabContainer extends HTMLElement {
-	static _meta = {
-		tag: 'tab-container',
-		template: document.createElement('template')
-	};
-	#root;
-	#slot;
+const __tab_conntainer__ = 'tab-container';
+if (typeof customElements.get(__tab_conntainer__) === 'undefined') {
+	const __template__ = document.createElement('template');
 
-	constructor() {
-		super();
-		this.#root = this.attachShadow({mode: 'closed'});
-		this.#root.appendChild(TabContainer._meta.template.content.cloneNode(true));
-		this.#slot = this.#root.querySelector('slot');
-	}
-	get #slotted() {
-		const nodes = this.#slot.assignedNodes();
-		return {
-			tabHeaders: nodes.filter(n => n.nodeName.toLowerCase() === 'tab-headers').at(0) ?? null,
-			tabSections: nodes.filter(n => n.nodeName.toLowerCase() === 'tab-sections').at(0) ?? null
-		};
-	}
-	connectedCallback() {
-		this.#slot.addEventListener('slotchange', _ => {
-			const s = this.#slotted;
-			if(s.tabHeaders !== null) {
-				s.tabHeaders.onchange = ({detail}) => {
-					s.tabSections?.setAttribute('selected', detail);
-					this.dispatchEvent(new CustomEvent('change', { detail: detail }));
-				};
-			}
-			s.tabSections?.setAttribute('selected', s.tabHeaders?.selected ?? 0);
-		});
-	}
-	// disconnectedCallback() {}
-	// adoptedCallback() {}
+	const __template_file__ = String(new URL(import.meta.url + '/../' + __tab_conntainer__ + '.html'));
+	__template__.innerHTML = await fetch(__template_file__).then(html => html.text().catch(e => 'Unable to load: ' + __template_file__));
 
-	// static get observedAttributes() {
-	// 	return [];
-	// }
-	// attributeChangedCallback(attrName, oldVal, newVal) {}
+	class TabContainer extends HTMLElement {
+		#root;
+		#slot;
+
+		constructor() {
+			super();
+			this.#root = this.attachShadow({ mode: 'closed' });
+			this.#root.appendChild(__template__.content.cloneNode(true));
+			this.#slot = this.#root.querySelector('slot');
+		}
+		get #slotted() {
+			const nodes = this.#slot.assignedNodes();
+			return {
+				tabHeaders: nodes.filter(n => n.nodeName.toLowerCase() === 'tab-headers').at(0) ?? null,
+				tabSections: nodes.filter(n => n.nodeName.toLowerCase() === 'tab-sections').at(0) ?? null
+			};
+		}
+		connectedCallback() {
+			this.#slot.addEventListener('slotchange', _ => {
+				const s = this.#slotted;
+				if (s.tabHeaders !== null) {
+					s.tabHeaders.onchange = ({ detail }) => {
+						s.tabSections?.setAttribute('selected', detail);
+						this.dispatchEvent(new CustomEvent('change', { detail: detail }));
+					};
+				}
+				s.tabSections?.setAttribute('selected', s.tabHeaders?.selected ?? 0);
+			});
+		}
+		// disconnectedCallback() {}
+		// adoptedCallback() {}
+
+		// static get observedAttributes() {
+		// 	return [];
+		// }
+		// attributeChangedCallback(attrName, oldVal, newVal) {}
+	}
+
+	customElements.define(__tab_conntainer__, TabContainer);
 }
-TabContainer._meta.template.innerHTML = await fetch(String(new URL(import.meta.url + '/../tab-container.html'))).then(html => html.text());
-Object.freeze(TabContainer);
-customElements.define(TabContainer._meta.tag, TabContainer);
--- tabs/src/tab-sections.js
+++ tabs/src/tab-sections.js
@@ -1,67 +1,70 @@
-class TabSections extends HTMLElement {
-	static _meta = {
-		tag: 'tab-sections',
-		template: document.createElement('template')
-	};
-	#root;
-	#slot;
+const __tab_sections__ = 'tab-sections';
+if (typeof customElements.get(__tab_sections__) === 'undefined') {
+	const __template__ = document.createElement('template');
 
-	constructor() {
-		super();
-		this.#root = this.attachShadow({mode: 'closed'});
-		this.#root.appendChild(TabSections._meta.template.content.cloneNode(true));
-		this.#slot = this.#root.querySelector('slot');
-	}
-	get #slottedSections() {
-		return this.#slot.assignedNodes().filter(n => n.nodeName.toLowerCase() === 'section');
-	}
-	connectedCallback() {
-		this.#slot.addEventListener('slotchange', _ => {
-			for(let [i, s] of Object.entries(this.#slottedSections)) {
-				s.dataset.index = i;
-			}
-		});
-		this.#changeSelected();
-	}
-	#changeSelected() {
-		const sections = this.#slottedSections;
-		if(sections.length === 0) { return; }
-		const selected = this.selected % sections.length;
-		for (let [i, h] of Object.entries(sections)) {
-			if(i == selected) { // Don't change == to ===;
-				h.setAttribute('data-selected', '');
-			} else {
-				h.removeAttribute('data-selected');
-			}
+	const __template_file__ = String(new URL(import.meta.url + '/../' + __tab_sections__ + '.html'));
+	__template__.innerHTML = await fetch(__template_file__).then(html => html.text().catch(e => 'Unable to load: ' + __template_file__));
+
+	class TabSections extends HTMLElement {
+		#root;
+		#slot;
+
+		constructor() {
+			super();
+			this.#root = this.attachShadow({ mode: 'closed' });
+			this.#root.appendChild(__template__.content.cloneNode(true));
+			this.#slot = this.#root.querySelector('slot');
 		}
-	}
-	get selected() {
-		const index = parseInt(this.getAttribute("selected"));
-		if(isNaN(index) || index < 0) { return 0; }
-		return index;
-	}
-	set selected(index) {
-		index = parseInt(index);
-		if(index === NaN || index < 0) { index = 0; }
-		this.setAttribute("selected", index);
-	}
-	// disconnectedCallback() {}
-	// adoptedCallback() {}
-
-	static get observedAttributes() {
-		return ["selected"];
-	}
-	attributeChangedCallback(attrName, oldVal, newVal) {
-		// console.log("Changed", attrName, ":", oldVal, "->", newVal);
-		switch (attrName) {
-			case 'selected':
-				if (parseInt(oldVal) !== parseInt(newVal)) {
-					this.#changeSelected();
+		get #slottedSections() {
+			return this.#slot.assignedNodes().filter(n => n.nodeName.toLowerCase() === 'section');
+		}
+		connectedCallback() {
+			this.#slot.addEventListener('slotchange', _ => {
+				for (let [i, s] of Object.entries(this.#slottedSections)) {
+					s.dataset.index = i;
 				}
-				break;
+			});
+			this.#changeSelected();
+		}
+		#changeSelected() {
+			const sections = this.#slottedSections;
+			if (sections.length === 0) { return; }
+			const selected = this.selected % sections.length;
+			for (let [i, h] of Object.entries(sections)) {
+				if (i == selected) { // Don't change == to ===;
+					h.setAttribute('data-selected', '');
+				} else {
+					h.removeAttribute('data-selected');
+				}
+			}
+		}
+		get selected() {
+			const index = parseInt(this.getAttribute("selected"));
+			if (isNaN(index) || index < 0) { return 0; }
+			return index;
+		}
+		set selected(index) {
+			index = parseInt(index);
+			if (index === NaN || index < 0) { index = 0; }
+			this.setAttribute("selected", index);
+		}
+		// disconnectedCallback() {}
+		// adoptedCallback() {}
+
+		static get observedAttributes() {
+			return ["selected"];
+		}
+		attributeChangedCallback(attrName, oldVal, newVal) {
+			// console.log("Changed", attrName, ":", oldVal, "->", newVal);
+			switch (attrName) {
+				case 'selected':
+					if (parseInt(oldVal) !== parseInt(newVal)) {
+						this.#changeSelected();
+					}
+					break;
+			}
 		}
 	}
+
+	customElements.define(__tab_sections__, TabSections);
 }
-TabSections._meta.template.innerHTML = await fetch(String(new URL(import.meta.url + '/../tab-sections.html'))).then(html => html.text());
-Object.freeze(TabSections);
-customElements.define(TabSections._meta.tag, TabSections);
